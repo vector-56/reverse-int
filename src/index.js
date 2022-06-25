e06a68f608ed6d3014ecf74cd1ab9ba4be6c86c7
Sergey <vector-56@yandex.ru>
2022-06-25 17:03:16 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let moduleN = Math.abs(n)
+    let toStringN = String(moduleN);
+    let reversedN = toStringN.split('').reverse().join('');
+    return reversedN;
 }
